f1def4341e767a080bb43bff9356d77630ab0a25
Joe Wilson <wilsonmangunsong@gmail.com>
2023-04-11 21:52:32 +0700

    cangkem

--- diary_api/main.go
+++ diary_api/main.go
@@ -1,16 +1,21 @@
 package main
 
 import (
+	"diary_api/controller"
 	"diary_api/database"
+	"diary_api/middleware"
 	"diary_api/model"
+	"fmt"
 	"log"
 
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
 func main() {
 	loadEnv()
 	loadDatabase()
+	serveApplication()
 }
 
 func loadDatabase() {
@@ -24,4 +29,20 @@ func loadEnv() {
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
+}
+
+func serveApplication() {
+	router := gin.Default()
+
+	publicRoutes := router.Group("/auth")
+	publicRoutes.POST("/register", controller.Register)
+	publicRoutes.POST("/login", controller.Login)
+
+	protectedRoutes := router.Group("/api")
+	protectedRoutes.Use(middleware.JWTAuthMiddleware())
+	protectedRoutes.POST("/entry", controller.AddEntry)
+	protectedRoutes.GET("/entry", controller.GetAllEntries)
+
+	router.Run(":8000")
+	fmt.Println("Server running on port 8000")
 }
